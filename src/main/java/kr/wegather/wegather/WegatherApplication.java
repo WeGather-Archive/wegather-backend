98563178af9d8a1ffafecea010c5d7cab01283c5
mathpaul3 <mathpaul3@gmail.com>
2022-07-23 16:46:14 +0900

    Initialize

---
+++ src/main/java/kr/wegather/wegather/WegatherApplication.java
@@ -0,0 +1,13 @@
+package kr.wegather.wegather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegatherApplication.class, args);
+	}
+
+}
