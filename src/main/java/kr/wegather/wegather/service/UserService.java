5d11a57df5428687860d39fc550039b9b5bbcb66
mathpaul3 <mathpaul3@gmail.com>
2022-07-24 16:56:20 +0900

    DB service

--- src/main/java/kr/wegather/wegather/service/UserService.java
+++ src/main/java/kr/wegather/wegather/service/UserService.java
@@ -1,2 +1,39 @@
-package kr.wegather.wegather.service;public class UserService {
+package kr.wegather.wegather.service;
+
+import kr.wegather.wegather.domain.User;
+import kr.wegather.wegather.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    /* 회원가입 */
+    @Transactional
+    public Long register(User user) {
+        validateDuplicateUser(user); // 회원 검증
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        // 파라미터 검사
+        List<User> findUsers = userRepository.findByEmail(user.getEmail());
+        if (findUsers.size() > 0) {
+            throw new IllegalStateException("User Already Exists");
+        }
+    }
+
+    /* 회원 조회 */
+    public User findOne(Long userId) {
+        return userRepository.findOne(userId);
+    }
+
 }
--- src/test/java/kr/wegather/wegather/service/UserServiceTest.java
+++ src/test/java/kr/wegather/wegather/service/UserServiceTest.java
@@ -1,4 +1,68 @@
+package kr.wegather.wegather.service;
+
+import kr.wegather.wegather.domain.SchoolDept;
+import kr.wegather.wegather.domain.User;
+import kr.wegather.wegather.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
 import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
 public class UserServiceTest {
-  
+
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
+
+
+    @Test
+    public void registration() throws Exception {
+        // given
+        Random random = new Random();
+        SchoolDept schoolDept = new SchoolDept();
+        schoolDept.setId(6478L);
+        User user = new User();
+        user.setSchoolDept(schoolDept);
+        user.setName("엄호용");
+        user.setEmail("mathpaul" + Integer.toString(random.nextInt()) + "@gmail.com");
+        user.setNickname("mathpaul3");
+        user.setPassword("password");
+
+        // when
+        Long savedId = userService.register(user);
+
+        // then
+        assertEquals(user, userRepository.findOne(savedId));
+    }
+
+    @Test
+    // 중복된 유저를 생성하는 경우 Test가 fail 해야 한다!
+    // java.lang.IllegalStateException: User Already Exists가 뜬다면 정상
+    public void registerDuplicated() throws Exception {
+        // given
+        SchoolDept schoolDept = new SchoolDept();
+        schoolDept.setId(6478L);
+        User user = new User();
+        user.setSchoolDept(schoolDept);
+        user.setName("엄호용");
+        user.setEmail("mathpaul3@gmail.com");
+        user.setNickname("mathpaul3");
+        user.setPassword("password");
+
+        // when
+        userService.register(user);
+
+        // then
+        fail("Exception Not DETECTED!!");
+    }
 }
