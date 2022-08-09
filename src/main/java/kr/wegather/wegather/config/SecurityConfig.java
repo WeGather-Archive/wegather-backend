62676a200b0c5e175bac0d6b187fc73d2e12f545
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-08-09 18:51:06 +0900

    [fix]: spring security CORS 이슈 수정

--- src/main/java/kr/wegather/wegather/config/SecurityConfig.java
+++ src/main/java/kr/wegather/wegather/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import kr.wegather.wegather.handler.CustomAuthenticationSuccessHandler;
 import kr.wegather.wegather.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
@@ -31,6 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.csrf().disable()
+				.cors().configurationSource(corsConfigurationSource())
+				.and()
 				.authorizeRequests()
 					.antMatchers("/user/**", "/login/**").authenticated() // /user/** 만 로그인 필요
 				.anyRequest().permitAll() // 나머지는 허용
@@ -39,9 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.usernameParameter("email")
 				.passwordParameter("password")
 				.loginProcessingUrl("/auth/login")
-				.defaultSuccessUrl("/login/success")
+//				.defaultSuccessUrl("/login/success")
 //				.failureUrl("/auth/fail")
-//				.successHandler(customAuthenticationSuccessHandler)
+				.successHandler(customAuthenticationSuccessHandler)
 				.failureHandler(customAuthenticationFailureHandler)
 				.and()
 				.logout()
@@ -52,6 +59,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(authService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.addAllowedOriginPattern("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
 
