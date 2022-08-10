b5bbfa83bf87749b70f8a05bdf08ef7cfe4809fe
keepgoing-hyeonsoo <chs98105@gmail.com>
2022-08-10 09:41:27 +0900

    [wip]

--- src/main/java/kr/wegather/wegather/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/kr/wegather/wegather/handler/CustomAuthenticationSuccessHandler.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//		System.out.println("success");
 		response.sendRedirect("/login/success");
 	}
 }
